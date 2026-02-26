9537daeb3ce0c24587c98ef39c9c787b42e04396
joan <joan.calderon@correounivalle.edu.co>
2026-02-26 12:22:44 -0500

    Pagina de pago exitoso

---
+++ netlify/functions/create-checkout.js
@@ -0,0 +1,32 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+exports.handler = async (event) => {
+    if (event.httpMethod !== "POST") {
+        return { statusCode: 405, body: "Method not allowed" };
+    }
+
+    try {
+        const { priceID } = JSON.parse(event.body);
+
+        const session = await stripe.checkout.sessions.create({
+            payment_method_types: ['card'],
+            line_items: [{
+                price: priceID,
+                quantity: 1
+            }],
+            mode: 'payment',
+            success_url: `${process.env.URL_SITE}/success.html`,
+            cancel_url: `${process.env.URL_SITE}/index.html`,
+        });
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ url: session.url }),
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: error.message }),
+        };
+    }
+};
