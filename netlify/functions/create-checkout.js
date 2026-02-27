414d9f520b091dc7a5c1397a0e28fc494e1b1d29
joan <joan.calderon@correounivalle.edu.co>
2026-02-26 20:12:21 -0500

    ass

--- netlify/functions/create-checkout.js
+++ netlify/functions/create-checkout.js
@@ -7,6 +7,7 @@ exports.handler = async (event) => {
 
     try {
         const { priceId } = JSON.parse(event.body);
+        console.log("Cuerpo recibido:", body);
 
         const session = await stripe.checkout.sessions.create({
             payment_method_types: ['card'],
